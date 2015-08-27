b67cf0c194eb4a2d9236b9afcb967bcecded9361
nantanits <nantanit.sut@mahidol.ac.th>
2015-08-27 16:08:01 +0700

    Version updated to 1.0.

--- build/html/_static/bizstyle.js
+++ build/html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("Nantanit Profile o.1 documentation");
+        $("li.nav-item-0 a").text("Nantanit Profile 1.0 documentation");
     }
 });
